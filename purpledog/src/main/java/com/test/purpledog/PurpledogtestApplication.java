e186df6e2b8ca1ed7b2ac0eb96f4b68a16671be0
SheepEatLion <gowjr207@gmail.com>
2022-03-01 12:39:40 +0900

    feat: spring boot set up
    - 기본환경세팅
    - Springboot + JPA

---
+++ purpledog/src/main/java/com/test/purpledog/PurpledogtestApplication.java
@@ -0,0 +1,13 @@
+package com.test.purpledog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurpledogtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurpledogtestApplication.class, args);
+	}
+
+}
---
+++ purpledog/src/test/java/com/test/purpledog/PurpledogtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.purpledog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurpledogtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
