f0af5ad474c9c2d71b3234af76be123745bf1cf3
SheepEatLion <gowjr207@gmail.com>
2022-03-01 13:56:15 +0900

    fix: 회원가입 메서드
    - 컨트롤러에 RequestBody 어노테이션 추가
    - 도메인에 기본생성자 롬복 어노테이션 추가

--- purpledog/src/main/java/com/test/purpledog/controller/UserController.java
+++ purpledog/src/main/java/com/test/purpledog/controller/UserController.java
@@ -4,6 +4,7 @@ import com.test.purpledog.dto.SignUpReq;
 import com.test.purpledog.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/user")
-    public void signUp(SignUpReq signUpReq){
+    public void signUp(@RequestBody SignUpReq signUpReq){
 
         userService.signUp(signUpReq);
 
--- purpledog/src/main/java/com/test/purpledog/domain/User.java
+++ purpledog/src/main/java/com/test/purpledog/domain/User.java
@@ -10,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Getter
+@NoArgsConstructor
 @Entity
 public class User {
 
